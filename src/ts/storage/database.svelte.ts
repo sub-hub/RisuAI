fbd5168710e1895aafd4103fc820dd88b496d252
kwaroran <amasio1234@proton.me>
2026-06-11 03:36:11 +0900

    chore: update version to 2026.6.102 in configuration and source files

--- src/ts/storage/database.svelte.ts
+++ src/ts/storage/database.svelte.ts
@@ -22,7 +22,7 @@ import {
 } from '../chatLoadPages';
 
 //APP_VERSION_POINT is to locate the app version in the database file for version bumping
-export let appVer = "2026.6.101" //<APP_VERSION_POINT>
+export let appVer = "2026.6.102" //<APP_VERSION_POINT>
 export let webAppSubVer = ''
 
 
